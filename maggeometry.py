ba78535c0a205abcfe5e45775cbfa502e8d32d46
Nelson Ooi <nelsonwmooi@gmail.com>
2023-06-22 14:51:47 -0400

    Updates

--- maggeometry.py
+++ maggeometry.py
@@ -5,10 +5,16 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import matplotlib.pyplot as plt
 
+"""
+Br must be given in mT
+so 1.09 T is 1.09e3 mT!!!!!!
+"""
+
+
 # define geometry with cylinders along circumference
 # used in Ruster and Malinowski
 def cyl_ring(n_magnets, br, c_diameter, c_height, st_rad, dist, show=False):
-    
+    assert br > 1e2
     magnets = magpy.Collection()
     theta_range = np.linspace(0, 2*np.pi, n_magnets)
     for theta in theta_range:
@@ -21,6 +27,7 @@ def cyl_ring(n_magnets, br, c_diameter, c_height, st_rad, dist, show=False):
 
 # define halbach cylinder geometry
 def halbach_cylinder(Br, c_d, c_h, D, n, alternate=False):
+    assert Br > 1e2
     magnets = magpy.Collection()
     if alternate:
         n = 2*n
@@ -54,7 +61,8 @@ def halbach_cylinder(Br, c_d, c_h, D, n, alternate=False):
 #     magpy.show(magnets, style_magnetization_show=True, backend='plotly')
     return magnets
 
-def four_coils(Br, innerrad1, innerrad2, width, thickness, dist1, dist2, show=False):
+def four_rings(Br, innerrad1, innerrad2, width, thickness, dist1, dist2, show=False):
+    assert Br > 1e2
     magnets = magpy.Collection()
     ring2_top = CylinderSegment(magnetization=(0, 0, Br),
                                dimension=(innerrad2, innerrad2 + width, thickness, 0, 360),
@@ -71,4 +79,75 @@ def four_coils(Br, innerrad1, innerrad2, width, thickness, dist1, dist2, show=Fa
     magnets.add(ring2_top).add(ring2_bot).add(ring1_top).add(ring1_bot)
     if show:
         magpy.show(magnets, backend='plotly')
+    return magnets
+
+def three_rings(Br, innerrad1, innerrad2, width, thickness, dist, show=False):
+    assert Br > 1e2
+    magnets = magpy.Collection()
+    ring2_top = CylinderSegment(magnetization=(0, 0, Br),
+                               dimension=(innerrad2, innerrad2 + width, thickness, 0, 360),
+                               position = (0, 0, +dist))
+    ring1 = CylinderSegment(magnetization=(0, 0, Br),
+                               dimension=(innerrad1, innerrad1 + width, thickness, 0, 360),
+                               position = (0, 0, 0))
+    ring2_bot = CylinderSegment(magnetization=(0, 0, Br),
+                               dimension=(innerrad2, innerrad2 + width, thickness, 0, 360),
+                               position = (0, 0, -dist))
+    magnets.add(ring2_top).add(ring2_bot).add(ring1)
+    if show:
+        magpy.show(magnets, backend='plotly')
+    return magnets
+
+def two_rings(Br, innerrad, width, thickness, dist, show=False):
+    assert Br > 1e2
+    magnets = magpy.Collection()
+    ring_top = CylinderSegment(magnetization=(0, 0, Br),
+                               dimension=(innerrad, innerrad + width, thickness, 0, 360),
+                               position = (0, 0, +dist))
+    ring_bot = CylinderSegment(magnetization=(0, 0, Br),
+                               dimension=(innerrad, innerrad + width, thickness, 0, 360),
+                               position = (0, 0, -dist))
+    magnets.add(ring_top).add(ring_bot)
+    if show:
+        magpy.show(magnets, backend='plotly')
+    return magnets
+
+
+# define halbach cylinder geometry with noise in angle
+def noisy_halbach_cylinder(Br, c_d, c_h, D, n, mean_cyl_deg, sd_cyl_deg, mean_pos_deg, sd_pos_deg, alternate=False):
+    assert Br > 1e2
+    magnets = magpy.Collection()
+    if alternate:
+        n = 2*n
+    for i in range(n):
+        d_theta = np.random.default_rng().normal(mean_cyl_deg, sd_cyl_deg)
+        d_pos_ang = np.random.default_rng().normal(mean_pos_deg, sd_pos_deg)
+        
+        theta_i = np.radians(720/n * i + d_theta)
+        b_rem = (Br * np.cos(theta_i), Br * np.sin(theta_i), 0)
+        pos_ang = np.radians(360/n * i + d_pos_ang)
+        pos = (D/2 * np.cos(pos_ang), D/2 * np.sin(pos_ang), 0)
+        src_i = Cylinder(magnetization=b_rem, position=pos, dimension=(c_d, c_h))
+        if (i % 2 == 1):
+            magnets.add(src_i)
+        if (not alternate and i % 2 == 0):
+            magnets.add(src_i)
+            
+    magnets.rotate_from_angax(90, 'x')
+    magnets.rotate_from_angax(-90, 'y')
+#     magnets.rotate_from_angax(-90, 'z')
+    user_defined_style = {
+        'show': True,
+        "size": 0.1,
+        'color': {
+            'transition': 0,
+            'mode': 'tricolor',
+            'middle': 'white',
+            'north': 'magenta',
+            'south': 'turquoise',
+        },
+        "mode": "arrow+color",
+    }
+    magpy.defaults.display.style.magnet.magnetization = user_defined_style
+#     magpy.show(magnets, style_magnetization_show=True, backend='plotly')
     return magnets
--- maghelper.py
+++ maghelper.py
@@ -60,16 +60,10 @@ def plot_plane_field_strength(magnets, x_grid_bounds, y_grid_bounds, z_elev, plo
                              shared_yaxes=False
                             )
     
-#     fig1 = go.Figure(data=[go.Surface(x=grid[:,:,0], y=grid[:,:,1], z=Gz)])
-#     fig1.update_layout(title=f'Field strength in plane z={z_elev}', autosize=False,
-#                       width=500, height=500,
-#                       margin=dict(l=65, r=50, b=65, t=90))
-#     fig.show()
     fig_base.add_trace(
         go.Surface(x=grid[:,:,0], y=grid[:,:,1], z=Gz),
         row=1, col=1
     )
-    
     
     if plot_nonuniformity:
         mid_idx = int(len(ts_x)/2)
@@ -77,11 +71,6 @@ def plot_plane_field_strength(magnets, x_grid_bounds, y_grid_bounds, z_elev, plo
         G_center = Gz[mid_idx][mid_idy]
         G_non = np.abs((Gz - G_center)/G_center) * 100
         
-#         fig2 = go.Figure(data=[go.Surface(x=grid[:,:,0], y=grid[:,:,1], z=G_non)])
-#         fig2.update_layout(title=f'Field nonuniformity in plane z={z_elev}', autosize=False,
-#                           width=500, height=500,
-#                           margin=dict(l=65, r=50, b=65, t=90))
-#         fig2.show()
         fig_base.add_trace(
             go.Surface(x=grid[:,:,0], y=grid[:,:,1], z=G_non),
             row=1, col=2
@@ -94,10 +83,96 @@ def plot_plane_field_strength(magnets, x_grid_bounds, y_grid_bounds, z_elev, plo
     fig_base.update_traces(showscale=False)
     fig_base.show()
 
+    
+def get_field_on_axes(magnets, sensor_bounds, is_logscale=True):
+    x_sensor_bounds, y_sensor_bounds, z_sensor_bounds = sensor_bounds
+    # define sensor axis in 3d space
+    sens_res = 5000
+    x_senpos = np.linspace((x_sensor_bounds[0], 0, 0), (x_sensor_bounds[1], 0, 0), sens_res)
+    # initialize sensor
+    x_sen = magpy.Sensor(position=x_senpos)
+
+    y_senpos = np.linspace((0, y_sensor_bounds[0], 0 ), (0, y_sensor_bounds[1], 0), sens_res)
+    y_sen = magpy.Sensor(position=y_senpos)
+    
+    z_senpos = np.linspace((0, 0, z_sensor_bounds[0]), (0, 0, z_sensor_bounds[1]), sens_res)
+    z_sen = magpy.Sensor(position=z_senpos)
+    
+    # read from transverse and axial sensors
+    x_Bz = x_sen.getB(magnets).T[2]
+    y_Bz = y_sen.getB(magnets).T[2]
+    z_Bz = z_sen.getB(magnets).T[2]
+
+    # convert from returned mT readings to Gauss
+    mT_to_G = 10
+    x_Gz = x_Bz * mT_to_G
+    y_Gz = y_Bz * mT_to_G
+    z_Gz = z_Bz * mT_to_G
+    
+    # set plotting configurations
+    plt.rc('xtick', labelsize=12) 
+    plt.rc('ytick', labelsize=12)
+    plt.rcParams.update({'font.size': 12})
+    
+    fig_width = 15
+    fig, axs = plt.subplots(2, 3, figsize=(fig_width, 2/3*fig_width))
+    axs = axs.flat
+    
+    # 1. plot the B_z component on each of the three sensor axes
+    axs[0].set_ylabel("Gauss")
+    axs[0].plot(x_senpos[:, 0], x_Gz)
+    axs[0].set_title("x-axis $B_z$ Profile")
+    axs[0].grid(color='.9', which='major', axis='both', linestyle='-')
+
+    axs[1].set_ylabel("Gauss")
+    axs[1].plot(y_senpos[:, 1], y_Gz)
+    axs[1].set_title("y-axis $B_z$ Profile")
+    axs[1].grid(color='.9', which='major', axis='both', linestyle='-')
+
+    axs[2].set_ylabel("Gauss")
+    axs[2].plot(z_senpos[:, 2], z_Gz)
+    axs[2].set_title("z-axis $B_z$ Profile")
+    axs[2].grid(color='.9', which='major', axis='both', linestyle='-')
+    
+    # 2. get nonuniformity along each of the 3 axes
+    x_Gnon, x_min_nonuniformity, x_max_nonuniformity, x_av_nonuniformity = get_nonuniformity(x_Gz, "x", sens_res)
+    y_Gnon, y_min_nonuniformity, y_max_nonuniformity, y_av_nonuniformity = get_nonuniformity(y_Gz, "y", sens_res)
+    z_Gnon, z_min_nonuniformity, z_max_nonuniformity, z_av_nonuniformity = get_nonuniformity(z_Gz, "z", sens_res)
+    
+    # 3. set nonuniformity plots to logscale
+    if (is_logscale):
+        axs[3].set_yscale("log")
+        axs[4].set_yscale("log")
+        axs[5].set_yscale("log")
+    
+    
+    # 4. plot nonuniformity
+    axs[3].plot(x_senpos[:, 0], x_Gnon)
+    axs[3].grid(color='.9', which='major', axis='both', linestyle='-')
+    axs[3].set_ylim(x_min_nonuniformity - 1e-7, x_max_nonuniformity + 1e-7)
+    axs[3].set_title('x-axis $B_z$ Non-uniformity Profile')
+    
+    axs[4].plot(y_senpos[:, 1], y_Gnon)
+    axs[4].grid(color='.9', which='major', axis='both', linestyle='-')
+    axs[4].set_ylim(y_min_nonuniformity - 1e-7, y_max_nonuniformity + 1e-7)
+    axs[4].set_title('y-axis $B_z$ Non-uniformity Profile')
+    
+    axs[5].plot(z_senpos[:, 2], z_Gnon)
+    axs[5].grid(color='.9', which='major', axis='both', linestyle='-')
+    axs[5].set_ylim(z_min_nonuniformity - 1e-7, z_max_nonuniformity + 1e-7)
+    axs[5].set_title('z-axis $B_z$ Non-uniformity Profile')
+    
+    magpy.show(magnets, x_sen, y_sen, z_sen, style_magnetization_show=True, backend='plotly')
+    sq = 10
+    plot_plane_field_strength(magnets, [-sq, sq], [-sq, sq], 0)
+
+    plt.tight_layout()
+    plt.show()    
+
 """
 Set up axial and transverse sensors and plots the B-field measured
 """
-def get_field_on_axes(magnets, axial_sensor_length, transverse_sensor_length, uniformity_bound_t, uniformity_bound_ax, is_logscale=False):
+def get_field_on_2_axes(magnets, axial_sensor_length, transverse_sensor_length, uniformity_bound_t, uniformity_bound_ax, is_logscale=False):
     # define sensor and axis
     senpos_transverse = np.linspace((-transverse_sensor_length/2, 0, 0), (transverse_sensor_length/2, 0, 0), 5000)
     sen_t = magpy.Sensor(position=senpos_transverse)
@@ -193,14 +268,22 @@ def get_field_on_axes(magnets, axial_sensor_length, transverse_sensor_length, un
     plt.show()
 
 # return nonuniformity from single axis sensor about central point
-def get_nonuniformity(G_measured, direction):
-    maxG = max(G_measured)
-    G_center = G_measured.item(int(len(G_measured)/2))
+def get_nonuniformity(Gmag, direction, sens_res):
+    Gmax = max(Gmag)
+    Gcenter = Gmag.item(int(len(Gmag)/2))
+    Gnon = np.abs((Gmag - Gcenter)/Gcenter)
+    min_nonuniformity = np.min(Gnon)
+    max_nonuniformity = np.max(Gnon)
+    av_nonuniformity = np.sum(Gnon) / sens_res
+
     
-    print(f"Maximum B-field along {direction} direction: {round(maxG, 3)} G")
-    print(f"Central B-field along {direction} axis: {round(G_center, 3)} G")
-    nonuniformity = (G_measured - G_center) / G_center
-    return nonuniformity
+    print(f"On {direction}-axis:")
+    print(f"Maximum B-field: {round(Gmax, 3)} G")
+    print(f"Central B-field: {round(Gcenter, 3)} G")
+    print(f"Min nonuniformity: {min_nonuniformity}")
+    print(f"Max nonuniformity:{max_nonuniformity}")
+    print("\n")
+    return Gnon, min_nonuniformity, max_nonuniformity, av_nonuniformity
 
 # create sweep range centered on center value with provided step size and number of samples
 def centered_sweep_range(center, steps, dx):
