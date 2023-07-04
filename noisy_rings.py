a6443882db2a6ee243448ae094c00f7191ab1440
Nelson Ooi <nelsonwmooi@gmail.com>
2023-07-03 22:04:31 -0400

    randomized nelder-mead

---
+++ noisy_rings.py
@@ -0,0 +1,7 @@
+import numpy as np
+import magpylib as magpy
+from magpylib.magnet import Cuboid, Cylinder, CylinderSegment
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import matplotlib.pyplot as plt
+
